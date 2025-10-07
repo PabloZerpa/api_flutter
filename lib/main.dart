f111901d0061a84a655cd0c55884e11b17acf554
Pablo <pablozerpav@gmail.com>
2025-10-06 23:18:03 -0400

    Init project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:api_flutter/screens/api_search.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ApiSearch()
+    );
+  }
+}
---
+++ lib/screens/api_search.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ApiSearch extends StatefulWidget {
+  const ApiSearch({super.key});
+
+  @override
+  State<ApiSearch> createState() => _ApiSearchState();
+}
+
+class _ApiSearchState extends State<ApiSearch> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Api search"),
+      ),
+        body: Column(
+          children: [
+            
+          ],
+        ),
+      );
+  }
+}
